e528417b1856e4b4d7c97bbe56bbf34f8e5aa59f
tarasovc47 <tarasovc47@live.ru>
2022-02-28 17:53:09 +0500

    включил URL-manager и настроил подключение к БД

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=i2crm',
     'username' => 'root',
-    'password' => '',
+    'password' => 'RootPassword',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
